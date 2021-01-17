f36626c2da0405ca6ca6c2d5e483947591ccc453
Piper Dougherty <doughertypiper@gmail.com>
2021-01-17 18:09:08 -0500

    Fixing CORS.

--- server.js
+++ server.js
@@ -92,7 +92,7 @@ const init = async () => {
     routes: {
       cors: {
         origin: ['*'], // an array of origins or 'ignore'
-        headers: ['Authorization'], // an array of strings - 'Access-Control-Allow-Headers'
+        headers: ['Authorization', 'Content-Type'], // an array of strings - 'Access-Control-Allow-Headers'
         exposedHeaders: ['Accept'], // an array of exposed headers - 'Access-Control-Expose-Headers',
         additionalExposedHeaders: ['Accept'], // an array of additional exposed headers
         maxAge: 60,
